6711a984de92b34c30d76dd246631aaf75988e07
ZhukovGreen <zhukovgreen@icloud.com>
2019-04-25 16:20:47 +0200

    Update

--- time_tracking_via_gcal/handlers/settings.py
+++ time_tracking_via_gcal/handlers/settings.py
@@ -10,11 +10,11 @@ from ..models.dal import (
 from ..structs import States
 from ..app import dp
 from ..messages import (
-    S_REG,
-    S_SHOW_MSG,
-    S_FMT_MSG,
-    S_WRONG_FMT_MSG,
-    S_UPD_SUCCESS_MSG,
+    SETTINGS_REGEX,
+    SETTINGS_GET_MSG,
+    SETTING_FMT_MSG,
+    SETTINGS_WRONG_FMT_MSG,
+    SETTINGS_UPD_SUCCESS_MSG,
 )
 
 
@@ -40,7 +40,7 @@ async def report_settings_get(msg: types.Message):
         indent=SETTINGS_INDENT,
     ).strip(SETTINGS_STRIP)
     await msg.reply(
-        S_SHOW_MSG.format(settings=settings),
+        SETTINGS_GET_MSG.format(settings=settings),
         reply_markup=inl_keyboard,
     )
 
@@ -51,25 +51,29 @@ async def report_settings_edit_callback(
     state = dp.current_state()
     await state.set_state(States.EDIT.value)
     await callback.bot.send_message(
-        callback.from_user.id, text=S_FMT_MSG
+        callback.from_user.id, text=SETTING_FMT_MSG
     )
 
 
 async def report_settings_set(msg: types.Message):
     try:
         rate, currency, tags = re.fullmatch(
-            S_REG, msg.text
+            SETTINGS_REGEX, msg.text
         ).groups()
         tags = (
             tags.replace(" ", "").lower().split(",")
         )
     except:
-        await msg.reply(S_WRONG_FMT_MSG +" please:\n\n" + S_FMT_MSG)
+        await msg.reply(
+            SETTINGS_WRONG_FMT_MSG
+            + " please:\n\n"
+            + SETTING_FMT_MSG
+        )
     else:
         await update_report_settings(
             msg, rate, currency, tags
         )
-        await msg.reply(S_UPD_SUCCESS_MSG)
+        await msg.reply(SETTINGS_UPD_SUCCESS_MSG)
         await report_settings_get(msg)
         state = dp.current_state()
         await state.set_state(States.VIEWING.value)
--- time_tracking_via_gcal/messages.py
+++ time_tracking_via_gcal/messages.py
@@ -8,9 +8,7 @@ INTRO_MSG = (
 ALREADY_REGISTRED_USER_MSG = (
     "User with this id is already registered"
 )
-GCAL_BUILD_NOTIF_MSG = (
-    r"Google calendar was built for the user {user_id}"
-)
+GCAL_BUILD_NOTIF_MSG = r"Google calendar was built for the user {user_id}"
 CURRENT_STATE_MSG = r"Your current state is {state}"
 ENTER_CAL_ID_MSG = "Now, enter your calendar id"
 SMTH_WENT_WRONG_MSG = "Something went wrong"
@@ -20,12 +18,12 @@ SEND_JSON_MSG = (
 )
 YOU_CAN_USE_BOT_MSG = "Great. Now you can use the bot"
 
-S_REG = (
+SETTINGS_REGEX = (
     r"(\d+|(?:\d+\.\d+)) ([A-Z]{3}) *((?:.*, ?)*.*)"
 )
-S_SHOW_MSG = "Your settings are:\n\n{settings}"
+SETTINGS_GET_MSG = "Your settings are:\n\n{settings}"
 
-S_FMT_MSG = (
+SETTING_FMT_MSG = (
     "Enter new settings in the format like\n"
     "{hour_rate} {currency} '{tag1}','{tag2}'...'{tagX}'\n"
     "number of tags could be from 0 to any number."
@@ -34,5 +32,7 @@ S_FMT_MSG = (
     "40.0 EUR ,,sprint2-1-0\n"
     "1000 RUB"
 )
-S_WRONG_FMT_MSG = "Wrong format"
-S_UPD_SUCCESS_MSG = "Settings updated successfully"
+SETTINGS_WRONG_FMT_MSG = "Wrong format"
+SETTINGS_UPD_SUCCESS_MSG = (
+    "Settings updated successfully"
+)
